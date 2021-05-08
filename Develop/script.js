3ac3252ee5775257f62abe2be2ec766e5d7d6c52
Zohaib Ali <s.zohaibali3@gmail.com>
2021-05-08 10:18:45 -0400

    added logic to show password via criteria

--- Develop/script.js
+++ Develop/script.js
@@ -12,21 +12,28 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-
 generateBtn.addEventListener("click", writePassword);
 
+
 function generatePassword(){
+
+  // Ask for length - looping till correct info
   do {
     input = window.prompt("Lenght of password (  Min 8 and Max 128 characters )");
     passLength =  Number(input)
     if (!input || isNaN(passLength)) {
       alert("Please Input valid number");
+      loopVar=false;
+    } else if (passLength < 8 ||  passLength > 128 ){
+      alert("Please type number from 8 to 128 range")
+      loopVar=false;
+    } else {
+      loopVar= true;
     }
-  } while (passLength < 8 ||  passLength > 128 || !input || isNaN(passLength));
+  } while (loopVar==false);
  
-    
+ // Ask for criteria - looping till atleast one selected   
   do {
-  atLeastOne = false;
   passLower = window.confirm("Do you want lower case character?");
   passUpper = window.confirm("Do you want upper case character?");
   passNum = window.confirm("Do you want numbers?");
@@ -36,22 +43,36 @@ function generatePassword(){
        passNum     === false &&
        passSpecial === false) {
      alert("Have to select atleast one");
+     atLeastOne= false;
    }else {
      atLeastOne = true;
    }
  } while (atLeastOne === false );
-  
-  //Generate Random number
- var result           = [];
-    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    var charactersLength = characters.length;
+
+ //intailizing different characterset
+ var charUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+ var charLower = "abcdefghijklmnopqrstuvwxyz";
+ var charNum = "0123456789";
+ var charSpecial = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
+ var passwordString = ""
+
+ if (passUpper) passwordString = passwordString + charUpper
+ if (passLower) passwordString= passwordString +charLower
+ if (passNum) passwordString= passwordString +charNum
+ if (passSpecial) passwordString+= passwordString +charSpecial
+
+console.log(passUpper,passLower,passNum,passSpecial)
+console.log(passwordString)
+  //Generate Random password based on criteria
+    var ans= [];
+    var passwordStringLength = passwordString.length;
     for ( var i = 0; i < passLength; i++ ) {
-      result.push(characters.charAt(Math.floor(Math.random() * 
- charactersLength)));
+      ans.push(passwordString.charAt(Math.floor(Math.random() * passwordStringLength)));
    }
-   return result.join('');
-  console.log(passLength,passLower,passUpper,passNum,passSpecial);
+   return ans.join('');
+
   }
+    
 
 /*GIVEN I need a new, secure password
 WHEN I click the button to generate a password
